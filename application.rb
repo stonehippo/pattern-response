b6d8dc1786c83ba67e71d46bbca86ea905bf6695
George White <george@cantina.co>
2012-03-26 14:49:25 -0400

    Got the menu working
    Recreated the menu logic from the original PHP

--- application.rb
+++ application.rb
@@ -3,17 +3,32 @@ require './partials.rb'
 set :public_folder, File.dirname(__FILE__) + '/public'
 
 class Pattern
-  attr_accessor :folder_name
+  attr_accessor :folder_name, :items
+  def initialize(folder_name)
+    @folder_name = folder_name
+    # get the subfolders and files for the current pattern
+    @items = []
+    Dir.foreach(Dir.pwd + '/patterns/' + folder_name) do |filename|
+      next if filename =~ /^\./ # skip parent folders and hidden files
+      @items.push(filename)
+    end
+  end
 end
 
 helpers Sinatra::Partials
 helpers do
   def get_patterns
-
+    objects = []
+    Dir.foreach(Dir.pwd + '/patterns') do |filename|
+      next if filename =~ /^\./ # skip parent folders and hidden files
+      @foo = Pattern.new(filename)
+      objects.push(@foo)
+    end
+    return objects
   end
   
-  def remove_chars
-    
+  def remove_chars(value)
+    value.gsub(/[0-9. -]/,'').downcase
   end
   
   def print_html
